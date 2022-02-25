a952ae6db7e07a9cfc7fcc8b4e0759c71a656939
StormMata <storm.mata097@gmail.com>
2022-02-25 13:10:49 -0500

    25-Feb-22; 13:10

--- HWK2.py
+++ HWK2.py
@@ -11,51 +11,48 @@ import sklearn as sklearn
 import csv
 
 # ----------------------------------------------------- Import Data -----------------------------------------------------
-FullData  = pd.read_csv('train.csv')
-TrainData = FullData.iloc[0:1000, :]                                                 # Grad first 100 rows and all columns
+FullData  = pd.read_csv('train.csv')                                                # Import training data
+TrainData = FullData.iloc[0:1000, :]                                                # Grab first 1000 rows and all columns
 
 # ----------------------------------------------------- Clean Data ------------------------------------------------------
 
-data = TrainData.select_dtypes(include=[np.number]).interpolate().dropna(axis=1)
+data = TrainData.select_dtypes(include=[np.number]).interpolate().dropna(axis=1)    # Only include numbers
 
 # ----------------------------------------- Determine Correlation of Predictors -----------------------------------------
 
-Indices = list(range(5,39))
-
-# for i in Indices:
-# print("Regression with the top",i,"correlated variables")
 numeric = data.select_dtypes(include=[np.number])
 numeric.head()
 
 numeric = numeric.dropna(axis=0, how='any', thresh=None, subset=None, inplace=False)
 
-corr = numeric.corr()
-cols = corr['SalePrice'].sort_values(ascending=False)[:10].index
+corr = numeric.corr()                                                               # Get correlations of variables
+cols = corr['SalePrice'].sort_values(ascending=False)[:10].index                    # Sort top 10 correlated variables
 
-TrainX = data[cols]
+TrainX = data[cols]                                                                 # Grab 10 variables
 
-TrainY = TrainX['SalePrice']
+TrainY = TrainX['SalePrice']                                                        # Set dependent variable
 
-TrainX = TrainX.drop(['SalePrice'], axis=1)
+TrainX = TrainX.drop(['SalePrice'], axis=1)                                         # Remove dependent variable
 
 # --------------------------------------------------- Create a Model ----------------------------------------------------
-lr = linear_model.LinearRegression()
-model = lr.fit(TrainX, TrainY)
-TrainPredictions = model.predict(TrainX)
+
+lr = linear_model.LinearRegression()                                                # Set regression model
+model = lr.fit(TrainX, TrainY)                                                      # Define model from training data
+TrainPredictions = model.predict(TrainX)                                            # Predict dependent variables
 
 print(f"   Training R^2 is: {model.score(TrainX,TrainY)}")
 
 # ----------------------------------------------------- Test Model ------------------------------------------------------
 
-TestData = FullData.iloc[1001:1459, :]  
+TestData = FullData.iloc[1001:1459, :]                                              # Grab testing data; last 458 rows
 
-TestX = TestData[cols]
+TestX = TestData[cols]                                                              # Get same top correlated variables
 
-TestY = TestX['SalePrice']
+TestY = TestX['SalePrice']                                                          # Set dependent variable
 
-TestX = TestX.drop(['SalePrice'], axis=1)
+TestX = TestX.drop(['SalePrice'], axis=1)                                           # Remove dependent variable
 
-TestPredictions = model.predict(TestX)
+TestPredictions = model.predict(TestX)                                              # Use model to predict dependent variables
 print(f"   Testing  R^2 is: {model.score(TestX,TestY)}\n")
 sklearn.metrics.mean_squared_error(TestPredictions, TestY)
 
@@ -63,12 +60,12 @@ IDs = TestData['Id']
 print(IDs)
 print(TestPredictions)
 
-file = open("predictions.csv", "w")
+file = open("predictions.csv", "w")                                                 # Open csv file
 writer = csv.writer(file)
 
-writer.writerow(['Id', 'SalePrice'])
+writer.writerow(['Id', 'SalePrice'])                                                # Write headers
 
 for w in range(1001,1458):
-    writer.writerow([IDs[w], TestPredictions[w-1001]])
+    writer.writerow([IDs[w], TestPredictions[w-1001]])                              # Write IDs and predictions
 
 file.close()
